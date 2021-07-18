cb103e9a8307c7ac1cdfcfc17129e346f58adf7b
Seok Ryu <gaorida@gmail.com>
2021-07-18 16:40:18 +0900

    add GalleryPopup.vue

---
+++ src/components/GalleryPopup.vue
@@ -0,0 +1,63 @@
+<template>
+    <div id="gallery-popup" class="content-container pl-8 pr-8">
+<!--        <div> {{id}}</div>-->
+<!--        <div> {{model}}</div>-->
+        <v-carousel
+                v-model="model"
+                hide-delimiter-background
+                show-arrows-on-hover
+        >
+            <v-carousel-item
+                    v-for="(item, index) in items"
+                    :key="index"
+                    :src="item.src"
+            >
+            </v-carousel-item>
+        </v-carousel>
+    </div>
+</template>
+
+<script>
+    const GALLERY_FILE_LENGTH = 7;
+
+    const initGalleryData = () => {
+        const items = [];
+        const imagePath = 'images/gallery'
+        const imageExtension = 'jpg';
+
+        for (let i = 1; i <= GALLERY_FILE_LENGTH; i++) {
+            items.push({
+                src: `${imagePath}/p${i}.${imageExtension}`
+            })
+        }
+
+        return items;
+    }
+    // initGalleryData();
+
+    export default {
+        name: "GalleryPopup",
+        props: {
+          id: {
+              type: Number,
+              default: 0,
+              required: true
+          }
+        },
+        mounted() {
+          console.log(this.id)
+            this.model = this.id;
+        },
+        data: () => ({
+            model: 0,
+            // model: this.id,
+            items: initGalleryData(),
+        }),
+        methods: {
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
