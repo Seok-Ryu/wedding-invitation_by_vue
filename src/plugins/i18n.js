48aa5955eb48e7db271325930b5b696016d359d6
Seok Ryu <gaorida@gmail.com>
2021-09-01 00:55:47 +0900

    get locale 함수 추가

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -5,15 +5,26 @@ import { ko, en } from '@/locales';
 Vue.use(VueI18n);
 
 
+const getLocale = () => {
+    const temp = navigator.language || navigator.userLanguage;
+    const language = temp.toLowerCase();
+
+    if (language.includes('ko')) {
+        return 'ko'
+    }
+
+    return 'en'
+}
+
 const i18n = new VueI18n({
-    locale: 'ko',
+    locale: getLocale(),
     messages: { ko, en },
 });
 
 // for hot reload - https://vue-loader.vuejs.org/guide/hot-reload.html#state-preservation-rules
 if (module.hot) {
     module.hot.accept(['@/locales/ko', '@/locales/en'], () => {
-        i18n.setLocaleMessage('ko', ko);
+        i18n.setLocaleMessage('ko-KR', ko);
         i18n.setLocaleMessage('en', en);
     });
 }
